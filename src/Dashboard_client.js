a9af3d7edc46fc73ad34490732355510c1e965a6
Srushti Sachdev <srushti@Srushtis-MacBook-Pro.local>
2021-10-29 16:04:22 -0500

    initial commit

---
+++ src/Dashboard_client.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard_client(){
+    return (
+        <div>
+            <h1>hello client!</h1>
+        </div>
+    )
+}
+
+export default Dashboard_client
