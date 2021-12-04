508f7822c23e6d8331d18c973eb64ab3d5210376
shivam <gautamshivam25@gmail.com>
2021-12-04 17:30:55 -0600

    trader fix

--- src/components/trader/AllClients.js
+++ src/components/trader/AllClients.js
@@ -1,68 +1,59 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {UserContext} from '../../UserProvider'
 import { Container,Nav, Navbar, NavDropdown, Form, FormControl, Button, Breadcrumb } from 'react-bootstrap'
 import { Card, Row, Col, Table, ProgressBar} from 'react-bootstrap'
 
-class AllClients extends React.Component{
+const AllClients = (props) => {
 
-	 
-	 constructor(props) {
-	    super(props);
-	    this.state = {
-	    	allUsers : []
-	    };
+	const {user} = useContext(UserContext);
+	const [allUsers, setAllUsers] = useState([])
 
-	  }
-
-	  componentDidMount(){
-	  	fetch('http://localhost:5000/traders/2/clients')
+	useEffect(() => {
+		fetch(`/traders/${user.user_id}/clients`)
 	    .then(res => res.json())
 	    .then((data) => {
-	      this.setState({ allUsers: data }, () => console.log('data fetched' + data))
+			console.log('data fetched',data);
+	      	if(Array.isArray(data))setAllUsers(data);
 	    })
 	    .catch(console.log)
 
-	  }
+	}, [])
 
-
-    render(){
-    	return (
-    		
-			<div >
+	return (
+		<div >
+			<div>
+				<h4> All Clients </h4>
+				
 				<div>
-					<h4> All Clients </h4>
-					
-					<div>
-						<Table border='2'>
-							<thead>
-								<tr> 
-									<td>Client ID </td>
-									<td>Client Name </td>
-									<td>Email</td>
-									<td>Bitcoin Balance</td>
-									<td>Fiat Balance </td>
+					<Table border='2'>
+						<thead>
+							<tr> 
+								<td>Client ID </td>
+								<td>Client Name </td>
+								<td>Email</td>
+								<td>Bitcoin Balance</td>
+								<td>Fiat Balance </td>
+								
+							</tr>
+						</thead>
+						<tbody>
+							{allUsers.map(({ client_id, fname, email, phone, bitcoin_balance, fiat_balance, commission_value, commission_type }) => (
+								<tr>
+									<td> {client_id} </td>
+									<td> {fname} </td>
+									<td> {email} </td>
+									<td> {bitcoin_balance} </td>
+									<td> {fiat_balance} </td>
 									
 								</tr>
-							</thead>
-							<tbody>
-								{this.state.allUsers.map(({ client_id, fname, email, phone, bitcoin_balance, fiat_balance, commission_value, commission_type }) => (
-							        <tr>
-							        	<td> {client_id} </td>
-							        	<td> {fname} </td>
-							        	<td> {email} </td>
-							        	<td> {bitcoin_balance} </td>
-							        	<td> {fiat_balance} </td>
-							        	
-							        </tr>
-							      ))}
-							</tbody>
-						</Table>
-					</div>
-
+							  ))}
+						</tbody>
+					</Table>
 				</div>
+
 			</div>
-		    )
-    }
+		</div>
+	)
 }
 
 
--- src/components/trader/TraderReport.js
+++ src/components/trader/TraderReport.js
@@ -1,156 +1,161 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import {UserContext} from '../../UserProvider'
 import { Container,Nav, Navbar, NavDropdown, Form, FormControl, Button, Breadcrumb } from 'react-bootstrap'
 import { Card, Row, Col, Table, ProgressBar} from 'react-bootstrap'
 
-class TraderReport extends React.Component{
+const TraderReport = (props) => {
 
-	 constructor(props) {
-	    super(props);
-	    this.state = {
-	    	filterText: '',
-	    	selectValue: 'clientid',
-	    	bankReport : [],
-	    	bitcoinReport : [],
-	    	allUsers : []
-	    };
+	const {user} = useContext(UserContext);
+	const [filterText, setFilterText] = useState('');
+	const [selectValue, setSelectValue] = useState('');
+	const [bankReport, setBankReport] = useState([]);
+	const [bitcoinReport, setBitCoinReport] = useState([]);
+	const [allUsers, setAllUsers] = useState([]);
 
-	    this.handleChange = this.handleChange.bind(this);
-	    this.handleSelectChange = this.handleSelectChange.bind(this);
-	    this.handleSubmit = this.handleSubmit.bind(this);
 
-	  }
 
-	  handleChange(event) {
-	    this.setState({filterText: event.target.value});
-	  }
-
-	  handleSelectChange(event) {
-	    this.setState({selectValue: event.target.value});
-	    fetch('http://localhost:5000/clients')
+	
+	useEffect(() => {
+		fetch(`/traders/${user.user_id}/clients`)
 	    .then(res => res.json())
 	    .then((data) => {
-	      this.setState({ allUsers: data }, () => console.log('all users data fetched' + data))
+			console.log('all clients data fetched for trader:'+user.user_id, data)
+			if(Array.isArray(data))setAllUsers(data);
 	    })
-	  }
+	}, [])
 
-	  handleSubmit(event) {
-	    //alert('A name was submitted: ' + this.state.selectValue);
-	    var client_id = this.state.filterText;
-	    let filteredData;
-		
-	    // client_id = filteredData;
-
-	    fetch('http://localhost:5000/bank?client_id='+ this.state.filterText + '&trader_id=2')
+	const handleSelectChange = (event)  => {
+		setSelectValue(event.target.value);
+		fetch(`/bank?client_id='+ ${event.target.value} + '&trader_id=${user.user_id}`)
 	    .then(res => res.json())
 	    .then((data) => {
-	      this.setState({ bankReport: data }, () => console.log('data fetched' + data))
+			console.log('bank report fetched', data);
+			if(Array.isArray(data))setBankReport(data);
 	    })
-	    .catch(console.log)
 
-	   	fetch('http://localhost:5000/btc/trade?client_id='+ this.state.filterText + '&trader_id=2')
+	   	fetch(`/btc/trade?client_id='+ ${event.target.value} + '&trader_id=${user.user_id}`)
 	    .then(res => res.json())
 	    .then((data) => {
-	      this.setState({ bitcoinReport: data }, () => console.log('bitcoin data fetched' + data))
+			console.log('btc report fetched', data);
+			if(Array.isArray(data))setBitCoinReport(data);
 	    })
-	    .catch(console.log)
+	}
 
-	    console.log(this.state.bitcoinReport);
-	    event.preventDefault();
-	  }
 
-    render(){
-    	return (
-    		
-			<div >
+	const handleChange = (event) => {
+		setFilterText(event.target.value);
+	}
+	const handleSubmit = () => {
+	    fetch(`/bank?trader_id=${user.user_id}`)
+	    .then(res => res.json())
+	    .then((data) => {
+			console.log('bank report fetched', data);
+			if(Array.isArray(data)){
+				data = data.filter((item) => item.client_fname.toLowerCase().includes(filterText.toLowerCase()));
+				setBankReport(data);
+			}
+	    })
+
+	   	fetch(`/btc/trade?trader_id=${user.user_id}`)
+	    .then(res => res.json())
+	    .then((data) => {
+			console.log('btc report fetched', data);
+			data = data.filter((item) => item.client_fname.toLowerCase().includes(filterText.toLowerCase()));
+			if(Array.isArray(data))setBitCoinReport(data);
+	    })
+	}
+
+	return (
+		<div >
+			<div>
+				<h4> Client Report and History </h4>
+
+				<p> Filter Client Based on : </p>
+
+				<Form>
+					<select value={selectValue} onChange={handleSelectChange} >
+						{
+							allUsers.map((item) => (
+								<option value={item.client_id}>{item.fname} {item.lname}</option>
+	  						))
+						}
+					</select>
+
+					<br/><br/>
+					<label>
+					  Enter Name/Address: <br/>
+					  <input type="text" value={filterText} onChange={handleChange} />
+					</label><br/>
+					<input type="button" value="Submit" onClick={handleSubmit} />
+				</Form>
+
+				<h4> Report of Client : </h4>
+				
+				<h5> Bank Transaction </h5>
 				<div>
-					<h4> Client Report and History </h4>
-
-					<p> Filter Client Based on : </p>
-
-					<Form onSubmit={this.handleSubmit}>
-						<select value={this.state.selectValue} onChange={this.handleSelectChange} >
-						  <option value="clientid">Client ID</option>
-						  <option value="name">Name</option>
-						</select>
-
-						<br/><br/>
-						<label>
-				          Enter Name/Address: <br/>
-				          <input type="text" value={this.state.value} onChange={this.handleChange} />
-				        </label><br/>
-					  	
-					  	 <input type="submit" value="Submit" />
-					</Form>
-
-					<h4> Report of Client : </h4>
-					
-					<h5> Bank Transaction </h5>
-					<div>
-						<Table border='2'>
-							<thead>
-								<tr> 
-									<td>Client ID </td>
-									<td>Client Name </td>
-									<td>Date Of transaction</td>
-									<td>Transaction Type </td>
-									<td>Transaction Amount</td>
-									<td>Status </td>
+					<Table border='2'>
+						<thead>
+							<tr> 
+								<td>Client ID </td>
+								<td>Client Name </td>
+								<td>Date Of transaction</td>
+								<td>Transaction Type </td>
+								<td>Transaction Amount</td>
+								<td>Status </td>
+							</tr>
+						</thead>
+						<tbody>
+							{bankReport.map(({ tid, client_id, client_fname, timestamp, type, amount, status }) => (
+								<tr key={tid}>
+									<td> {client_id} </td>
+									<td> {client_fname} </td>
+									<td> {timestamp} </td>
+									<td> {type} </td>
+									<td> {amount} </td>
+									<td> {status} </td>
 								</tr>
-							</thead>
-							<tbody>
-								{this.state.bankReport.map(({ client_id, client_fname, timestamp, type, amount, status }) => (
-							        <tr>
-							        	<td> {client_id} </td>
-							        	<td> {client_fname} </td>
-							        	<td> {timestamp} </td>
-							        	<td> {type} </td>
-							        	<td> {amount} </td>
-							        	<td> {status} </td>
-							        </tr>
-							      ))}
-							</tbody>
-						</Table>
-					</div>
-
-					<br/><br/>
-					<br/><br/>
-					<h5> Bitcoin Transaction </h5>
-					<div>
-						<Table border='2'>
-							<thead>
-								<tr> 
-									<td>Client ID </td>
-									<td>Client Name </td>
-									<td>Date Of transaction</td>
-									<td>Transaction Type </td>
-									<td>Bitcoin Quantity</td>
-									<td>Bitcoin Rate </td>
-									<td>Commission Value</td>
-									<td>Commission Type</td>
-								</tr>
-							</thead>
-							<tbody>
-								{this.state.bitcoinReport.map(({ client_id, client_fname, timestamp, transaction_type, btc_qty, btc_rate, commission_value, commission_type }) => (
-							        <tr>
-							        	<td> {client_id} </td>
-							        	<td> {client_fname} </td>
-							        	<td> {timestamp} </td>
-							        	<td> {transaction_type} </td>
-							        	<td> {btc_qty} </td>
-							        	<td> {btc_rate} </td>
-							        	<td> {commission_value} </td>
-							        	<td> {commission_type} </td>
-							        </tr>
-							      ))}
-							</tbody>
-						</Table>
-					</div>
-
+							  ))}
+						</tbody>
+					</Table>
 				</div>
+
+				<br/><br/>
+				<br/><br/>
+				<h5> Bitcoin Transaction </h5>
+				<div>
+					<Table border='2'>
+						<thead>
+							<tr> 
+								<td>Client ID </td>
+								<td>Client Name </td>
+								<td>Date Of transaction</td>
+								<td>Transaction Type </td>
+								<td>Bitcoin Quantity</td>
+								<td>Bitcoin Rate </td>
+								<td>Commission Value</td>
+								<td>Commission Type</td>
+							</tr>
+						</thead>
+						<tbody>
+							{bitcoinReport.map(({ tid, client_id, client_fname, timestamp, transaction_type, btc_qty, btc_rate, commission_value, commission_type }) => (
+								<tr key={tid}>
+									<td> {client_id} </td>
+									<td> {client_fname} </td>
+									<td> {timestamp} </td>
+									<td> {transaction_type} </td>
+									<td> {btc_qty} </td>
+									<td> {btc_rate} </td>
+									<td> {commission_value} </td>
+									<td> {commission_type} </td>
+								</tr>
+							  ))}
+						</tbody>
+					</Table>
+				</div>
+
 			</div>
-		    )
-    }
+		</div>
+	)
 }
 
 
